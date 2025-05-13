e7a84906379ad227d23a89c8af26a79ae162882a
Molnár Attila <molnar.attila@szlgbp.hu>
2025-05-13 09:34:50 +0200

    legyen is valami

--- class.py
+++ class.py
@@ -1,5 +1,11 @@
 class Adat:
 
-    def __init__(self):
-        pass
+    def __init__(self, nev:str, magassag:int):
+        self.nev = nev
+        self.magassag = magassag
+
+    def __str__(self) -> str:
+        return self.nev
+
+
 
